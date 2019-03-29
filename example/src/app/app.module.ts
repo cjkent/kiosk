c895b969938309a7bc6db2138f18a75c07d89e35
Roman Kolacz <roman@kolacz.io>
2019-03-29 19:57:49 +0000

    Add Apache-2.0 license

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {INITIAL_STATE} from './store/state';
 import {TextComponent} from './text/text.component';
 import {Text2Component} from './text2/text2.component';
-import {StoreModule} from './store/module';
+import {StoreModule} from '../store/module';
 
 @NgModule({
   declarations: [
